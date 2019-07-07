8412cdca4329cd0ad985ff6b03dae3a14d08b9dc
BuruguEric <ericgburugu@gmail.com>
2019-07-07 17:07:49 +0300

    release

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -95,8 +95,12 @@
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required autocomplete="new-password">
-
+                                @if (!empty($password))
+                                <input id="password" type="password" value="{{ $password }} class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required autocomplete="new-password">
+                                @else
+                                <input id="password" type="password" value="{{ old('password') }} class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required autocomplete="new-password">
+                                @endif
+                                
                                 @if ($errors->has('password'))
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('password') }}</strong>
